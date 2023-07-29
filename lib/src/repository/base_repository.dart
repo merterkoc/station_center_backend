46ae16b8640d9de312082d4dab149e5a56dc14f6
mertyasarerkoc <mert.erkoc@finkoto.com>
2023-07-30 00:36:35 +0300

    baser repository added

---
+++ lib/src/repository/base_repository.dart
@@ -0,0 +1,11 @@
+import 'package:station_center_backend/environment/environment_service.dart';
+
+class BaseRepository {
+  final EnvironmentService env = EnvironmentService();
+
+  String get mongoDbStationCollectionName => env.mongoDbStationCollectionName!;
+
+  String get mongoDbUri => env.mongoDbUri!;
+
+  String get openChargeApiKey => env.openChargeApiKey!;
+}
